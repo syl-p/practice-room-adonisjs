109a0b6fd3bd382e61a373184f77b7ce5f52c1f3
sylvain <syl-p@ik.me>
2025-01-07 11:44:56 +0100

    feat: add alpinejs + dropdown comp ...

--- app/controllers/exercises/comments_controller.ts
+++ app/controllers/exercises/comments_controller.ts
@@ -1,39 +1,36 @@
-// import type { HttpContext } from '@adonisjs/core/http'
-
 import Comment from '#models/comment'
 import Exercise from '#models/exercise'
 import { newCommentValidator } from '#validators/comment'
 import { HttpContext } from '@adonisjs/core/http'
 import { errors } from '@vinejs/vine'
-import { STATUS_CODES } from 'node:http'
 
 export default class CommentsController {
-  async index({ params }: HttpContext) {
+  async index({ params, view }: HttpContext) {
     const comments = await Comment.query()
+      .preload('user')
       .where('commentableId', params.exercise_id)
       .andWhere('commentableType', 'Exercise')
       .orderBy('createdAt', 'desc')
 
-    return comments
+    return view.render('fragments/comments', { comments })
   }
 
   async store({ request, params, auth, view, session, response }: HttpContext) {
     const exercise = await Exercise.findByOrFail({ id: params.exercise_id })
-    const comment = new Comment()
-
-    if (auth.user) comment.userId = auth.user?.id
+    let comment = new Comment()
+    comment.userId = auth.user!.id
     comment.commentableType = 'Exercise'
     comment.commentableId = params.exercise_id
 
     try {
       const data = await request.validateUsing(newCommentValidator)
       comment.merge(data)
-      await exercise.related('comments').create(comment)
+      await exercise.related('comments').save(comment)
       await comment.load('user')
     } catch (error) {
       if (error instanceof errors.E_VALIDATION_ERROR) {
         session.flashValidationErrors(error)
-        response.status(500)
+        response.status(422)
       }
     }
 
